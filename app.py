a3bcb409e059de4f8551cd3121e8cfa20177f796
Shivbaba <shivbaba@macbookpro.attlocal.net>
2025-10-15 15:42:43 -0500

    updated app.py; created confirmation.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,8 +10,16 @@ def home():
 def menu():
     return render_template('menu.html')
 
-@app.route('/order')
+@app.route('/order', methods=['GET', 'POST'])
 def order():
+    if request.method == 'POST':
+        name = request.form['name']
+        cookie = request.form['cookie']
+        quantity = request.form['quantity']
+        pickup_time = request.form['pickup_time']
+
+        return render_template('confirmation.html', name=name, cookie=cookie, quantity=quantity, pickup_time=pickup_time)
+
     return render_template('order.html')
 
 if __name__ == '__main__':
